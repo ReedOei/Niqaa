401d34b533837c588035f85e701f50fff72b7282
Reed Oei <oei.reed@gmail.com>
2017-08-14 06:11:00 -0400

    More performance enchancements.

--- src/Controller/Physics.hs
+++ src/Controller/Physics.hs
@@ -22,7 +22,7 @@ collide (Circle x1 y1 r) (Rect x2 y2 w h _) =
           closestY = clamp y1 (y2 - h / 2) (y2 + h / 2)
 
 collide r1@(Rect x1 y1 w1 h1 a1) r2@(Rect x2 y2 w2 h2 a2)
-    | a1 == 0 && a2 == 0 = 
+   | abs a1 <= 0.01 && abs a2 <= 0.01 = 
         x1 - w1 / 2 <= x2 + w2 / 2 &&
         x1 + w1 / 2 >= x2 - w2 / 2 &&
         y1 - h1 / 2 <= y2 + h2 / 2 &&
@@ -31,7 +31,7 @@ collide r1@(Rect x1 y1 w1 h1 a1) r2@(Rect x2 y2 w2 h2 a2)
 
     where   -- Find corner's coords, treat as vector's
       -- UL: upper left, UR: upper right, BL: bottom left, BR: bottom right
-
+    
       corner_UL1 = V2D (rtf $ x1 + hyp1*cos(aUL1), rtf $ y1 + hyp1*sin(aUL1))
       corner_UR1 = V2D (rtf $ x1 + hyp1*cos(aUR1), rtf $ y1 + hyp1*sin(aUR1))
       corner_BL1 = V2D (rtf $ x1 + hyp1*cos(aBL1), rtf $ y1 + hyp1*sin(aBL1))
@@ -43,16 +43,19 @@ collide r1@(Rect x1 y1 w1 h1 a1) r2@(Rect x2 y2 w2 h2 a2)
       corner_BR2 = V2D (rtf $ x2 + hyp2*cos(aBR2), rtf $ y2 + hyp2*sin(aBR2))
 
 
-
-      aUL1 = atan(-h1/w1) + pi + a1
-      aUR1 = atan(h1/w1) + a1
-      aBL1 = atan(h1/w1) + pi + a1
-      aBR1 = atan(-h1/w1) + a1
-
-      aUL2 = atan(-h2/w2) + pi + a2
-      aUR2 = atan(h2/w2) + a2
-      aBL2 = atan(h2/w2) + pi + a2
-      aBR2 = atan(-h2/w2) + a2
+      -- atan is an odd function so we can only compute it once then use
+      -- atan (-x) == - atan x
+      atan1 = atan $ h1 / w1
+      aUL1 = -atan1 + pi + a1
+      aUR1 = atan1 + a1
+      aBL1 = atan1 + pi + a1
+      aBR1 = -atan1 + a1
+    
+      atan2 = atan $ h2 / w2
+      aUL2 = -atan2 + pi + a2
+      aUR2 = atan2 + a2
+      aBL2 = atan2 + pi + a2
+      aBR2 = -atan2 + a2
 
       hyp1 = sqrt((w1/2)^2 + (h1/2)^2)
       hyp2 = sqrt((w2/2)^2 + (h2/2)^2)
@@ -101,10 +104,6 @@ collide r1@(Rect x1 y1 w1 h1 a1) r2@(Rect x2 y2 w2 h2 a2)
 
 dot_prod :: V2D -> V2D -> Float
 dot_prod (V2D (x1,y1)) (V2D (x2,y2)) = x1*x2 + y1*y2
-
-
-magnitude :: V2D -> Float
-magnitude (V2D (q, r)) = sqrt(q^2 + r^2)
 
 
 overlap :: [Float] -> [Float] -> Bool
